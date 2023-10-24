0863e7089450a9263a868f2380b6655325954dca
Omri Ovadia <omriovadia8@gmail.com>
2023-10-24 18:01:42 +0300

    Created Prefab Art Replacer Tool

--- Assets/Scripts/ArtReplacerWindow.cs
+++ Assets/Scripts/ArtReplacerWindow.cs
@@ -1,32 +1,67 @@
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UI;
+using System.Linq;
 
-public class ArtReplacerWindow : EditorWindow
+public class ArtReplacerTool : EditorWindow
 {
+    private GameObject templatePrefab;
     private Texture2D newSpriteSheet;
+    private string prefabName = "NewPrefab";
 
     [MenuItem("Tools/Art Replacer")]
-    public static void ShowWindow()
+    static void Init()
     {
-        GetWindow<ArtReplacerWindow>("Art Replacer");
+        ArtReplacerTool window = (ArtReplacerTool)EditorWindow.GetWindow(typeof(ArtReplacerTool));
+        window.Show();
     }
 
-    private void OnGUI()
+    void OnGUI()
     {
-        GUILayout.Label("Art Replacement Tool", EditorStyles.boldLabel);
+        templatePrefab = (GameObject)EditorGUILayout.ObjectField("Template Prefab:", templatePrefab, typeof(GameObject), false);
+        newSpriteSheet = (Texture2D)EditorGUILayout.ObjectField("New SpriteSheet:", newSpriteSheet, typeof(Texture2D), false);
+        prefabName = EditorGUILayout.TextField("Prefab Name:", prefabName);
 
-        newSpriteSheet = (Texture2D)EditorGUILayout.ObjectField("New SpriteSheet", newSpriteSheet, typeof(Texture2D), false);
-
-        if (GUILayout.Button("Replace Art"))
+        if (GUILayout.Button("Replace Art & Save New Prefab"))
         {
             ReplaceArt();
         }
     }
 
-    void ReplaceArt()
+    private void ReplaceArt()
     {
-        // Replacement logic will go here.
-        Debug.Log("Replace button pressed!"); // For now, just log to see if it's working.
+        if (templatePrefab == null || newSpriteSheet == null)
+        {
+            Debug.LogError("Ensure both the Template Prefab and New SpriteSheet are set!");
+            return;
+        }
+
+        GameObject newInstance = Instantiate(templatePrefab);
+
+        string spriteSheetPath = AssetDatabase.GetAssetPath(newSpriteSheet);
+        Sprite[] sprites = AssetDatabase.LoadAllAssetsAtPath(spriteSheetPath).OfType<Sprite>().ToArray();
+
+        ReplaceSpritesInChildren(newInstance.transform, sprites);
+        string prefabPath = "Assets/Prefabs/" + prefabName + ".prefab";
+        PrefabUtility.SaveAsPrefabAsset(newInstance, prefabPath);
+        DestroyImmediate(newInstance);
     }
 
+    private void ReplaceSpritesInChildren(Transform parent, Sprite[] sprites)
+    {
+        foreach (Transform child in parent)
+        {
+            Image img = child.GetComponent<Image>();
+            if (img != null && img.sprite != null)
+            {
+                string currentSpriteName = img.sprite.name;
+                Sprite matchingSprite = sprites.FirstOrDefault(sprite => sprite.name == currentSpriteName);
+                if (matchingSprite != null)
+                {
+                    img.sprite = matchingSprite;
+                }
+            }
+            ReplaceSpritesInChildren(child, sprites);
+        }
+    }
 }
